08e61a22f90c0aea9950b9572b31a33c644c5af9
Yash Tiwari <yashvtiwari04@gmail.com>
2024-09-10 14:52:40 -0500

    inclass activity finished

--- src/ExampleApp.ts
+++ src/ExampleApp.ts
@@ -8,6 +8,12 @@ import * as gfx from 'gophergfx'
 
 export class ExampleApp extends gfx.GfxApp
 {   
+    bgColor = new gfx.Color(0,0,0);
+    
+    mouth = gfx.Geometry2Factory.createBox(1, 0.1);
+
+    elapsedTime = 0.0;
+
     // --- Create the ExampleApp class ---
     constructor()
     {
@@ -19,11 +25,32 @@ export class ExampleApp extends gfx.GfxApp
     // --- Initialize the graphics scene ---
     createScene(): void 
     {
+        this.renderer.background = this.bgColor;
+
+        const rEye = gfx.Geometry2Factory.createBox(0.1, 0.5);
+        const lEye = gfx.Geometry2Factory.createBox(0.1, 0.5);
+        const nose = gfx.Geometry2Factory.createBox(0.1, 0.2);
+    
+        rEye.position = new gfx.Vector2(-0.5, 0.5);
+        lEye.position = new gfx.Vector2(0.5, 0.5);
+        this.mouth.position = new gfx.Vector2(0, -0.5);
+
+        this.scene.add(rEye);
+        this.scene.add(lEye);
+        this.scene.add(nose);
+        this.scene.add(this.mouth);
     }
 
 
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
+        this.elapsedTime += deltaTime;
+        this.bgColor.b += 0.01;
+        this.bgColor.r += 0.001;
+        this.renderer.background = this.bgColor;
+
+        this.mouth.scale = new gfx.Vector2(1, Math.abs(Math.sin(this.elapsedTime)));
+
     }
 }
